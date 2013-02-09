0902d2d9f92b66a6c6058c26a899057aff34e3b6
Biswaranjan.Das <biswaranjan.das@gmail.com>
2013-02-09 20:55:50 +0000

    Adding Hierarchical Aggregated Test cases

--- testcore/com/biswa/ep/entities/AllTests.java
+++ testcore/com/biswa/ep/entities/AllTests.java
@@ -4,14 +4,16 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import com.biswa.ep.subscription.SubscriptionAttributeTest;
-
 @RunWith(Suite.class)
 @SuiteClasses({
 		LeafAttributeTest.class,
 		PrivateAttributeTest.class,
 		StatelessAttributeTest.class,
-		StaticAttributeTest.class})
+		StaticAttributeTest.class,
+		com.biswa.ep.entities.predicate.AllTests.class,
+		com.biswa.ep.entities.store.AllTests.class,
+		com.biswa.ep.entities.substance.AllTests.class,
+		com.biswa.ep.entities.transaction.AllTests.class})
 public class AllTests {
 
 }
