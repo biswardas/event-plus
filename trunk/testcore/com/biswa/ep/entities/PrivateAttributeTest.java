bf2461a82a8d9dadb3af716969346e7fad914049
biswaranjan.das@gmail.com <biswaranjan.das@gmail.com@837f84b3-34e9-d943-8c0c-9cbd1e6a6e68>
2013-02-09 18:40:46 +0000

    Add Private Attribute test case.

---
+++ trunk/testcore/com/biswa/ep/entities/PrivateAttributeTest.java
@@ -0,0 +1,56 @@
+package com.biswa.ep.entities;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PrivateAttributeTest{
+
+	@Test
+	public void testPropagate() {
+		PrivateAttribute privateAttribute = new PrivateAttribute("privateAttribute");
+		assertFalse(privateAttribute.propagate());
+	}
+
+	@Test
+	public void testDependsOn() {
+		PrivateAttribute privateAttribute = new PrivateAttribute("privateAttribute");
+		assertArrayEquals(PrivateAttribute.ZERO_DEPENDENCY,privateAttribute.dependsOn());
+	}
+
+	@Test
+	public void testRequiresStorage() {
+		PrivateAttribute privateAttribute = new PrivateAttribute("privateAttribute");
+		assertTrue(privateAttribute.requiresStorage());
+	}
+
+	@Test
+	public void testShouldInitializeOnInsert() {
+		PrivateAttribute privateAttribute = new PrivateAttribute("privateAttribute");
+		assertTrue(privateAttribute.initializeOnInsert());
+	}
+
+	@Test(expected=RuntimeException.class)
+	public void testSetPropagate() {
+		PrivateAttribute privateAttribute = new PrivateAttribute("privateAttribute");
+		privateAttribute.setPropagate(true);
+	}
+
+	@Test
+	public void testIsSubscription() {
+		PrivateAttribute privateAttribute = new PrivateAttribute("privateAttribute");
+		assertFalse(privateAttribute.isSubscription());
+	}
+
+	@Test
+	public void testIsStateless() {
+		PrivateAttribute privateAttribute = new PrivateAttribute("privateAttribute");
+		assertFalse(privateAttribute.isStateless());
+	}
+
+	@Test
+	public void testIsStatic() {
+		PrivateAttribute privateAttribute = new PrivateAttribute("privateAttribute");
+		assertFalse(privateAttribute.isStatic());
+	}
+}
