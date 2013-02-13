ef1895fe2b4e248bf8938dcb3d7bc9173ac1acff
biswaranjan.das@gmail.com <biswaranjan.das@gmail.com@837f84b3-34e9-d943-8c0c-9cbd1e6a6e68>
2013-02-13 00:51:35 +0000

    Javadocs TransportEntry

--- trunk/core/com/biswa/ep/entities/TransportEntry.java
+++ trunk/core/com/biswa/ep/entities/TransportEntry.java
@@ -7,51 +7,93 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.biswa.ep.entities.substance.Substance;
-
+/**Transport record to construct an entry in Sink container.
+ * 
+ * @author biswa
+ *
+ */
 public class TransportEntry implements Serializable {
-	public static final Map<Attribute,Substance> EMPTY_MAP = Collections.unmodifiableMap(new HashMap<Attribute,Substance>());
+	public static final Map<Attribute, Substance> EMPTY_MAP = Collections
+			.unmodifiableMap(new HashMap<Attribute, Substance>());
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -1620363036023519197L;
-	private final int externalIdentity;
-	//Each Attribute and substance are kept here for this record
-	protected Map<Attribute, Substance> containerEntryStore;
-	
-	/**Constructor used by Anonymous servers to make inserts into the container.
-	 * 
-	 * @param externalIdentity the id by which the rest of the world knows this.
+	/**
+	 * Container identifier
 	 */
-	public TransportEntry(int externalIdentity){
-		this(externalIdentity,EMPTY_MAP);
-	}
+	private final int externalIdentity;
 	
 	/**
-	 * 
-	 * @param externalIdentity the id by which the rest of the world knows this.
-	 * @param entryQualifier The qualifying attributes when the record is created.
+	 * Entry Attribute value pair required to create a logical entry.
 	 */
-	public TransportEntry(int externalIdentity,Map<Attribute, Substance> entryQualifier){
-		this.containerEntryStore=entryQualifier;
-		this.externalIdentity=externalIdentity;
+	protected Map<Attribute, Substance> containerEntryStore;
+
+	/**
+	 * Constructor used by Anonymous servers to make inserts into the container.
+	 * 
+	 * @param externalIdentity
+	 *            the id by which the rest of the world knows this.
+	 */
+	public TransportEntry(int externalIdentity) {
+		this(externalIdentity, EMPTY_MAP);
 	}
-	
+
+	/**
+	 * 
+	 * @param externalIdentity
+	 *            the id by which the rest of the world knows this.
+	 * @param entryQualifier
+	 *            The qualifying attributes when the record is created.
+	 */
+	public TransportEntry(int externalIdentity,
+			Map<Attribute, Substance> entryQualifier) {
+		this.containerEntryStore = entryQualifier;
+		this.externalIdentity = externalIdentity;
+	}
+
+	/**
+	 * Identity of the entry as known to source containers. Sink containers may
+	 * use the identity as is or map it to newly generated id.
+	 * 
+	 * @return identity
+	 */
 	public int getIdentitySequence() {
 		return externalIdentity;
 	}
 
+	/**
+	 * Map containing attribute value pairs to construct a logical entity in the
+	 * sink container. Treat this like arguments to a constructor.
+	 * 
+	 * @return Map<Attribute, Substance>
+	 */
 	public Map<Attribute, Substance> getEntryQualifier() {
 		return containerEntryStore;
 	}
-	private Object readResolve() throws ObjectStreamException{
-		if(containerEntryStore==null){
+
+	/**
+	 * If received null over the network reconstruct the object with EMPTY_MAP.
+	 * 
+	 * @return Object
+	 * @throws ObjectStreamException
+	 */
+	private Object readResolve() throws ObjectStreamException {
+		if (containerEntryStore == null) {
 			containerEntryStore = EMPTY_MAP;
 		}
 		return this;
 	}
 
-	private Object writeReplace() throws ObjectStreamException{
-		if(containerEntryStore==EMPTY_MAP){
+	/**
+	 * Don't bother to send over an empty map over network. Replace it with
+	 * null.
+	 * 
+	 * @return Object
+	 * @throws ObjectStreamException
+	 */
+	private Object writeReplace() throws ObjectStreamException {
+		if (containerEntryStore == EMPTY_MAP) {
 			containerEntryStore = null;
 		}
 		return this;
@@ -65,5 +107,5 @@ public class TransportEntry implements Serializable {
 				.append(containerEntryStore).append("]");
 		return builder.toString();
 	}
-	
+
 }
