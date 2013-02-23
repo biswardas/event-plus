991245ad2fb1ec13d67575eb5d5fc8981514baf1
Biswaranjan.Das@gmail.com <Biswaranjan.Das@gmail.com@837f84b3-34e9-d943-8c0c-9cbd1e6a6e68>
2013-02-23 14:04:06 +0000

    Fix PrivateAttribute

--- trunk/testcore/com/biswa/ep/entities/PrivateAttributeTest.java
+++ trunk/testcore/com/biswa/ep/entities/PrivateAttributeTest.java
@@ -2,55 +2,74 @@ package com.biswa.ep.entities;
 
 import static org.junit.Assert.*;
 
+
 import org.junit.Test;
 
+import com.biswa.ep.entities.substance.Substance;
+class XPrivate extends PrivateAttribute{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4936361919515510979L;
+
+	public XPrivate(String name) {
+		super(name);
+	}
+
+	@Override
+	protected Substance evaluate(Attribute attribute,
+			ContainerEntry containerEntry) throws Exception {
+		return null;
+	}
+	
+}
 public class PrivateAttributeTest{
 
 	@Test
 	public void testPropagate() {
-		PrivateAttribute privateAttribute = new PrivateAttribute("privateAttribute");
+		PrivateAttribute privateAttribute = new XPrivate("privateAttribute");
 		assertFalse(privateAttribute.propagate());
 	}
 
 	@Test
 	public void testDependsOn() {
-		PrivateAttribute privateAttribute = new PrivateAttribute("privateAttribute");
+		PrivateAttribute privateAttribute = new XPrivate("privateAttribute");
 		assertArrayEquals(PrivateAttribute.ZERO_DEPENDENCY,privateAttribute.dependsOn());
 	}
 
 	@Test
 	public void testRequiresStorage() {
-		PrivateAttribute privateAttribute = new PrivateAttribute("privateAttribute");
+		PrivateAttribute privateAttribute = new XPrivate("privateAttribute");
 		assertTrue(privateAttribute.requiresStorage());
 	}
 
 	@Test
 	public void testShouldInitializeOnInsert() {
-		PrivateAttribute privateAttribute = new PrivateAttribute("privateAttribute");
+		PrivateAttribute privateAttribute = new XPrivate("privateAttribute");
 		assertTrue(privateAttribute.initializeOnInsert());
 	}
 
 	@Test
 	public void testSetPropagate() {
-		PrivateAttribute privateAttribute = new PrivateAttribute("privateAttribute");
+		PrivateAttribute privateAttribute = new XPrivate("privateAttribute");
 		privateAttribute.setPropagate(true);
 	}
 
 	@Test
 	public void testIsSubscription() {
-		PrivateAttribute privateAttribute = new PrivateAttribute("privateAttribute");
+		PrivateAttribute privateAttribute = new XPrivate("privateAttribute");
 		assertFalse(privateAttribute.isSubscription());
 	}
 
 	@Test
 	public void testIsStateless() {
-		PrivateAttribute privateAttribute = new PrivateAttribute("privateAttribute");
+		PrivateAttribute privateAttribute = new XPrivate("privateAttribute");
 		assertFalse(privateAttribute.isStateless());
 	}
 
 	@Test
 	public void testIsStatic() {
-		PrivateAttribute privateAttribute = new PrivateAttribute("privateAttribute");
+		PrivateAttribute privateAttribute = new XPrivate("privateAttribute");
 		assertFalse(privateAttribute.isStatic());
 	}
 }
