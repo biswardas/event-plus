f1e3074ad410d893c1e0cbae72753f880600bb09
Biswaranjan.Das@gmail.com <Biswaranjan.Das@gmail.com@837f84b3-34e9-d943-8c0c-9cbd1e6a6e68>
2013-03-01 17:14:52 +0000

    EPATTRIBUTE ALLOWED ON METHOD AND LET CODE GENERATOR TO OVERRIDE PROPAGATE METHOD.

--- trunk/depl/com/biswa/ep/annotations/EPAttribute.java
+++ trunk/depl/com/biswa/ep/annotations/EPAttribute.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target( { ElementType.FIELD, ElementType.TYPE })
+@Target( { ElementType.FIELD,ElementType.METHOD, ElementType.TYPE })
 public @interface EPAttribute {
 	EPAttrType type() default EPAttrType.Member;
 
