12a4c0621ef8aa1bd4947268e2d10b99b5b360e8
Biswaranjan.Das@gmail.com <Biswaranjan.Das@gmail.com@837f84b3-34e9-d943-8c0c-9cbd1e6a6e68>
2013-03-11 03:32:21 +0000

    Transaction Tracker TimeOut Test Improvised.

--- trunk/testcore/com/biswa/ep/entities/transaction/TransactionTrackerTimeOutTest.java
+++ trunk/testcore/com/biswa/ep/entities/transaction/TransactionTrackerTimeOutTest.java
@@ -1,129 +1,55 @@
 package com.biswa.ep.entities.transaction;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 import java.util.ArrayList;
 import java.util.Properties;
 import java.util.concurrent.Semaphore;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
 
 import com.biswa.ep.entities.ConcreteContainer;
+import com.biswa.ep.entities.ConnectionEvent;
 import com.biswa.ep.entities.ContainerTask;
 import com.biswa.ep.entities.PropertyConstants;
 
 public class TransactionTrackerTimeOutTest {
 
-	private TransactionTracker getTransactionTracker(String delay){
+	private ConcreteContainer getTransactionTracker(String delay){
 		Properties props = new Properties();
 		props.put(PropertyConstants.TRAN_TIME_OUT, delay);
-		ConcreteContainer cs = new ConcreteContainer("TestSchema",props){
-			@Override
-			public boolean isConnected() {
-				return true;
-			}	
-		};
-		TransactionAdapter transactionAdapter = cs.agent(); 
-		return transactionAdapter.transactionTracker;
+		ConcreteContainer cs = new ConcreteContainer("TestSchema",props);
+		cs.agent().addSource(new ConnectionEvent(cs.getName(), cs.getName()));
+		cs.agent().connected(new ConnectionEvent(cs.getName(), cs.getName()));
+		cs.agent().waitForEventQueueToDrain();
+		return cs; 
 	}
 
 	@Test
 	public void testTimeout() throws Exception{
+		final ConcreteContainer cs=getTransactionTracker("2000");
+		Assert.assertEquals(2000, cs.getTimeOutPeriodInMillis());
+		long startTime = System.currentTimeMillis();
+		cs.agent().beginTran(new TransactionEvent("TestSchema", 100));
 		final Semaphore s = new Semaphore(1);
 		s.drainPermits();
-		final TransactionTracker transactionTracker=getTransactionTracker("5000");
-		final ArrayList<String> al = new ArrayList<String>();
-		transactionTracker.transactionAdapter.getEventCollector().execute(new Runnable(){
+		cs.agent().invokeOperation(new ContainerTask() {
 			@Override
-			public void run() {
-//				transactionTracker.addSource("A",2);
-//				transactionTracker.addSource("X",1);
-//				transactionTracker.trackBeginTransaction(100, "A");
-//				assertEquals(100, transactionTracker.getCurrentTransactionID());
-//				transactionTracker.trackBeginTransaction(101, "X");
-				transactionTracker.addOperation(100, new ContainerTask(){
-					@Override
-					public void runtask() {
-						al.add("a");
-					}
-				});
-				assertTrue(al.contains("a"));
-
-				transactionTracker.addOperation(101, new ContainerTask(){
-					@Override
-					public void runtask() {
-						al.add("b");
-						
-					}
-				});
-				assertFalse(al.contains("b"));
-				transactionTracker.addOperation(101, new ContainerTask(){
-					@Override
-					public void runtask() {
-						al.clear();
-						s.release();
-					}
-				});
-				//transactionTracker.trackCommitTransaction(101, "X");
-				try {
-					Thread.sleep(transactionTracker.transactionAdapter.getTimeOutPeriodInMillis());
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}		
-			}			
-		});
-		s.acquire();
-		assertTrue(al.isEmpty());
-	}
-
-
-	@Test
-	public void testNoTimeout() throws Exception{
-		final TransactionTracker transactionTracker=getTransactionTracker("15000");
-		final ArrayList<String> al = new ArrayList<String>();
-		transactionTracker.transactionAdapter.getEventCollector().execute(new Runnable(){
-			@Override
-			public void run() {
-//				transactionTracker.addSource("A",2);
-//				transactionTracker.addSource("X",1);
-//				transactionTracker.trackBeginTransaction(100, "A");
-//				transactionTracker.trackBeginTransaction(101, "X");
-				transactionTracker.addOperation(100, new ContainerTask(){
-					@Override
-					public void runtask() {
-						al.add("a");
-					}
-				});
-				assertTrue(al.contains("a"));
-
-				transactionTracker.addOperation(101, new ContainerTask(){
-					@Override
-					public void runtask() {
-						al.add("b");
-						
-					}
-				});
-				//transactionTracker.trackCommitTransaction(101, "X");
-				transactionTracker.addOperation(101, new ContainerTask(){
-					@Override
-					public void runtask() {
-						al.clear();						
-					}
-				});
-
-				try {
-					Thread.sleep(10000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}		
+			protected void runtask() throws Throwable {
+				s.release();
 			}
 		});
-		Thread.sleep(12000);
-		assertFalse(al.isEmpty());
-		Integer[] currentlyinProgress = {100,101};
-		assertArrayEquals(currentlyinProgress, transactionTracker.transactionsInProgress());
+		s.acquireUninterruptibly();
+		long endTime = System.currentTimeMillis();
+		Assert.assertEquals(0, cs.agent().getCurrentTransactionID());
+		Assert.assertTrue((endTime-startTime)>2000);
+	}
+	
+	@Test
+	public void testNoTimeout() throws Exception{
+		final Semaphore s = new Semaphore(1);
+		s.drainPermits();
+		final ConcreteContainer cs=getTransactionTracker("0");
+		Assert.assertEquals(0, cs.getTimeOutPeriodInMillis());
 	}
 }
