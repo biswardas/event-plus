fe8a5d1b7f98abee94a1a8480c9e782d049b81ae
Biswaranjan.Das <biswaranjan.das@gmail.com>
2013-03-20 01:13:21 +0000

    1/10 Fixing examples

--- examples/StockQuoteAnalyzer/src/generator/Portfolio.java
+++ examples/StockQuoteAnalyzer/src/generator/Portfolio.java
@@ -1,27 +1,31 @@
 package generator;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.SynchronousQueue;
 
 public class Portfolio {
-	private SynchronousQueue<Map<Object, Object>> queue = null;
-	public void init(final SynchronousQueue<Map<Object, Object>> queue) throws InterruptedException {
-		this.queue=queue;
-		put("IBM",100.0,200.0);
-		put("CSCO",500.0,20.0);
-		put("NVDA",1000.0,15.0);
-		put("ORCL",400.0,34.0);
-		put("IBM",100.0,200.0);
-		put("CSCO",500.0,20.0);
-		put("NVDA",1000.0,15.0);
-		put("ORCL",400.0,34.0);
+	public void init(Connection connection) throws SQLException {
+		CallableStatement call = connection.prepareCall("call StockQuoteAnalyzer.InputStocks.insert(?)");
+		put(call,"IBM",100.0,200.0);
+		put(call,"CSCO",500.0,20.0);
+		put(call,"NVDA",1000.0,15.0);
+		put(call,"ORCL",400.0,34.0);
+		put(call,"IBM",100.0,200.0);
+		put(call,"CSCO",500.0,20.0);
+		put(call,"NVDA",1000.0,15.0);
+		put(call,"ORCL",400.0,34.0);
+		call.executeBatch();
+		connection.commit();
 	}
-	private void put(String stock,Double quantity,Double tranPrice) throws InterruptedException{
-		HashMap<Object,Object> hm2 = new HashMap<Object,Object>();
-		hm2.put("symbol",stock);
-		hm2.put("quantity",quantity);
-		hm2.put("tranPrice",tranPrice);
-		queue.put(hm2);
+	private void put(CallableStatement call,String stock,Double quantity,Double tranPrice) throws SQLException{
+		call.setObject("symbol",stock);
+		call.setObject("quantity",quantity);
+		call.setObject("tranPrice",tranPrice);
+		call.addBatch();
 	}
+	public void terminate(){}
 }
