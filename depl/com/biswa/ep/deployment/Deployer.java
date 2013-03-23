e7125ae0e2742da1eaa4a29d7dcf9963352725ff
Biswaranjan.Das <biswaranjan.das@gmail.com>
2013-03-23 19:57:26 +0000

    2/10 Refactoring and Cleanups for ForkJoin Deployment handler implementation.

--- depl/com/biswa/ep/deployment/Deployer.java
+++ depl/com/biswa/ep/deployment/Deployer.java
@@ -5,17 +5,13 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.management.ManagementFactory;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
-import java.util.Map.Entry;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.Future;
 
 import javax.management.MBeanServer;
 import javax.management.ObjectName;
@@ -28,16 +24,17 @@ import com.biswa.ep.NamedThreadFactory;
 import com.biswa.ep.UncaughtExceptionHandler;
 import com.biswa.ep.deployment.mbean.ConMan;
 import com.biswa.ep.deployment.mbean.ConManMBean;
-import com.biswa.ep.deployment.util.Container;
 import com.biswa.ep.deployment.util.Context;
-import com.biswa.ep.deployment.util.Listen;
 import com.biswa.ep.deployment.util.Param;
+import com.biswa.ep.discovery.Binder;
+import com.biswa.ep.discovery.RegistryHelper;
 
-public class Deployer extends UncaughtExceptionHandler{
-	private static final String MANUAL = "manual";
+public class Deployer extends UncaughtExceptionHandler{	
+	private static final ContainerManager containerManager = new ContainerManager();
 
-	private static final String DEPLOYMENT_ORDER = "deployment.order";
-
+	private static EPDeployerImpl deployerImpl = null;
+	private static EPDeployer exportedDeployer = null;
+	
 	private static final String DEPLOYMENT_DESC = "deployment.desc";
 
 	final static ExecutorService deployer = Executors
@@ -63,9 +60,23 @@ public class Deployer extends UncaughtExceptionHandler{
 			deployer.execute(new Runnable() {				
 				@Override
 				public void run() {
-					System.err.println("No deployment descripter found standing by. (-Ddeployment.desc=$fileName)");					
+					if(getSufix()!=null){
+						System.err.println("Starting as Slave. (-Ddeployment.desc=$fileName)");
+						try {
+							Binder binder = RegistryHelper.getBinder();
+							deployerImpl = new EPDeployerImpl();
+							exportedDeployer = (EPDeployer) UnicastRemoteObject
+									.exportObject(deployerImpl, 0);
+							binder.bind(getSufix(), exportedDeployer);
+						} catch (RemoteException e) {
+							throw new RuntimeException(e);
+						}
+					}else{
+						System.err.println("Slave mode not enabled.(-Dep.engine.name=$engineName) (-Ddeployment.desc=$fileName)");		
+						System.exit(0);
+					}
 				}
-			});						
+			});
 		}
 	}
 
@@ -73,7 +84,7 @@ public class Deployer extends UncaughtExceptionHandler{
 	public static void deploy(String fileName) throws JAXBException {
 		System.out.println("Attempting to deploy "+fileName);
 		Context context = buildContext(fileName);
-		deploy(context);
+		deploy(context,false);
 		System.out.println(fileName+" deployed.");
 	}
 	
@@ -109,96 +120,20 @@ public class Deployer extends UncaughtExceptionHandler{
 		return (Context) rootObj.getValue();
 	}
 
-	public static void deploy(final Context context) {
-		deployer.execute(new DeploymentTask(context));
+	public static Future<?> deploy(final Context context,boolean sorted) {
+		containerManager.registerWithManagementServer(context);
+		return deployer.submit(new DeploymentTask(context,containerManager,sorted));
 	}
 
-
-	static List<Container> getOrderedContainers(final Context context) {
-		Properties props = getProperties(context.getParam());
-		List<Container> containers = context.getContainer();
-		if(MANUAL.equals(props.getProperty(DEPLOYMENT_ORDER))){
-			return manualOrdering(containers);
-		}
-		return autoOrdering(context, containers);
-	}
-
-	private static List<Container> manualOrdering(List<Container> containers) {
-		Collections.sort(containers, new Comparator<Container>() {
-			@Override
-			public int compare(Container o1, Container o2) {
-				return o1.getOrder() - o2.getOrder();
-			}
-		});
-		return containers;
-	}
-
-	private static List<Container> autoOrdering(final Context context,
-			List<Container> containers) {
-		HashMap<Container,AtomicInteger> containerDependencyMap = new HashMap<Container,AtomicInteger>();
-		for(Container container:containers){
-			addContainer(containerDependencyMap, container,context);
-		}
-		List<Map.Entry<Container,AtomicInteger>> sortedContainer = new ArrayList<Map.Entry<Container,AtomicInteger>>();
-		sortedContainer.addAll(containerDependencyMap.entrySet());
-		Collections.sort(sortedContainer, new Comparator<Map.Entry<Container,AtomicInteger>>() {
-			@Override
-			public int compare(Map.Entry<Container,AtomicInteger> o1, Map.Entry<Container,AtomicInteger> o2) {
-				return o1.getValue().get() - o2.getValue().get();
-			}
-		});
-		containers.clear();
-		for(Map.Entry<Container, AtomicInteger> entry:sortedContainer){
-			containers.add(entry.getKey());
-		}
-		return containers;
-	}
-
-	private static void addContainer(
-			HashMap<Container, AtomicInteger> containerDependencyMap,
-			Container container,final Context context) {
-		if(!containerDependencyMap.containsKey(container)){
-			AtomicInteger atomInt = new AtomicInteger(1);
-			for(Listen listen:container.getListen()){
-				if(listen.getContext().equals(context.getName())){
-					AtomicInteger nested = null;
-					String dependsOn = listen.getContainer();
-					if((nested=getNestedWeight(containerDependencyMap, dependsOn))==null){
-						for(Container searchedContainer: context.getContainer()){
-							if(searchedContainer.getName().equals(dependsOn)){
-								addContainer(containerDependencyMap,searchedContainer,context);
-								nested=containerDependencyMap.get(searchedContainer);
-								break;
-							}
-						}
-						if(nested==null){
-							throw new RuntimeException("Unresolved container:"+dependsOn);
-						}
-					}
-					atomInt.addAndGet(nested.get());
-				}
-			}
-			containerDependencyMap.put(container, atomInt);
-		}
-	}
-
-
-	private static AtomicInteger getNestedWeight(
-			HashMap<Container, AtomicInteger> containerDependencyMap,
-			String dependsOn) {
-		for(Entry<Container,AtomicInteger> oneEntry: containerDependencyMap.entrySet()){
-			if(oneEntry.getKey().getName().equals(dependsOn)){
-				return oneEntry.getValue();
-			}
-		}
-		return null;
-	}
-	
 	public static Properties getProperties(List<Param> params) {
 		Properties props = new Properties();
 		for(Param oneParam:params){
 			props.put(oneParam.getName(), oneParam.getValue());
 		}
 		return props;
+	}
+
+	public static String getSufix() {
+		return System.getProperty("ep.engine.name");
 	}
 }
