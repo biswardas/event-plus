91a3e05b741a052c02edbd4a8e8c0db7f10a6b74
Biswaranjan.Das <biswaranjan.das@gmail.com>
2013-03-29 23:54:30 +0000

    8/10 Remove any direct acccess to registry adding controlling methods to Helper to make sure registry intigrity is protected at all times.

--- depl/com/biswa/ep/discovery/RMITransactionGenerator.java
+++ depl/com/biswa/ep/discovery/RMITransactionGenerator.java
@@ -1,9 +1,6 @@
 package com.biswa.ep.discovery;
 
 import java.rmi.RemoteException;
-import java.rmi.registry.Registry;
-
-import javax.rmi.PortableRemoteObject;
 /**Reference implementation of the transaction generator service.
  * 
  * @author biswa
@@ -12,13 +9,7 @@ import javax.rmi.PortableRemoteObject;
 public class RMITransactionGenerator implements TransactionGenerator,com.biswa.ep.entities.transaction.TransactionGenerator {
 	private static TransactionGenerator transactiongenerator;
 	public RMITransactionGenerator(){
-		try{
-			Registry registry = RegistryHelper.getRegistry();
-			Object obj = registry.lookup(TransactionGenerator.TRANSACTION_GENERATOR);
-			transactiongenerator = (TransactionGenerator) PortableRemoteObject.narrow(obj, TransactionGenerator.class);
-		}catch(Exception e){
-			throw new RuntimeException(e);
-		}
+		transactiongenerator = RegistryHelper.getTransactionGenerator();
 	}
 	
 	@Override
