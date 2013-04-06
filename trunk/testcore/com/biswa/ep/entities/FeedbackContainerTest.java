253b9d4baf28eb66279e10cbbf97561ef68dd598
Biswaranjan.Das@gmail.com <Biswaranjan.Das@gmail.com@837f84b3-34e9-d943-8c0c-9cbd1e6a6e68>
2013-04-06 23:10:29 +0000

    IF non transactional source send events to throttled aware container then periodic flush cycle will take care of the changes. All feedback related complexities are hereby delegated to feedback tracker including timeouts.

--- trunk/testcore/com/biswa/ep/entities/FeedbackContainerTest.java
+++ trunk/testcore/com/biswa/ep/entities/FeedbackContainerTest.java
@@ -36,63 +36,6 @@ public class FeedbackContainerTest {
 	public void tearDown() throws Exception {
 		source.agent().destroy();
 	}
-	
-	@Test
-	public void testNonTransactionalOrigin() throws InterruptedException{
-		final AtomicInteger atom = new AtomicInteger();
-		final Semaphore s = new Semaphore(1);
-		s.drainPermits();
-		Agent agent = new Agent(new ConcreteContainer(SINK, new Properties())){
-			@Override
-			public void connected(ConnectionEvent ce) {
-				System.out.println("Received ConnectedEvent:"+ce);
-				super.connected(ce);
-			}
-
-			@Override
-			public void entryAdded(ContainerEvent ce) {
-				System.out.println("Received ContainerEvent:"+ce);
-			}
-
-			@Override
-			public void entryRemoved(ContainerEvent ce) {
-				System.out.println("Received ContainerEvent:"+ce);
-			}
-
-			@Override
-			public void entryUpdated(ContainerEvent ce) {
-				System.out.println("Received ContainerEvent:"+ce);
-			}
-
-			@Override
-			public void beginTran(TransactionEvent te) {
-				System.out.println("Received Transaction Started:"+te);
-				super.beginTran(te);
-			}
-
-			@Override
-			public void commitTran(TransactionEvent te) {
-				System.out.println("Received Transaction Completed:"+te);
-				s.release();
-				super.commitTran(te);
-			}
-			
-		};
-		source.agent().connect(new ConnectionEvent(SOURCE,SINK,agent));
-		agent.addFeedbackAgent(new FeedbackAgentImpl(SINK,source.agent()));		
-		source.agent().entryAdded(new ContainerInsertEvent(SOURCE,new TransportEntry(10000),0));
-		source.agent().entryAdded(new ContainerInsertEvent(SOURCE,new TransportEntry(10001),0));
-		
-		source.agent().entryRemoved(new ContainerDeleteEvent(SOURCE,10001,0));		
-		source.agent().entryUpdated(new ContainerUpdateEvent(SOURCE,10000,attr,InvalidSubstance.INVALID_SUBSTANCE,0));
-		
-		s.acquireUninterruptibly();
-		source.agent().beginTran(new TransactionEvent(SOURCE,987654));		
-		source.agent().entryAdded(new ContainerInsertEvent(SOURCE,new TransportEntry(1234567),0));
-		source.agent().commitTran(new TransactionEvent(SOURCE,987654));
-		s.acquireUninterruptibly();
-		Thread.sleep(1000);
-	}
 
 	@Test
 	public void testTransactionalCollapsing() throws InterruptedException{
@@ -162,51 +105,54 @@ public class FeedbackContainerTest {
 	@Test
 	public void testThrottleQueued() throws InterruptedException{
 		final Semaphore tran = new Semaphore(1);
-		final int lock=100000;
+		final int lock=1000;
 		final CountDownLatch s = new CountDownLatch(lock*2);
-		Agent agent = new Agent(new ConcreteContainer(SINK, new Properties())){
+		ConcreteContainer sink = new ConcreteContainer(SINK, new Properties()){
 			@Override
-			public void connected(ConnectionEvent ce) {
-				System.out.println("Received ConnectedEvent:"+ce);
-				super.connected(ce);
+			protected void dispatchBeginTransaction(){
+				System.out.println("Begining Transaction:"+agent().getCurrentTransactionID());
+				super.dispatchBeginTransaction();
 			}
-
 			@Override
-			public void entryAdded(ContainerEvent ce) {
-				System.out.println("Received ContainerEvent:"+ce.getIdentitySequence());
+			protected void dispatchCommitTransaction(){
+				super.dispatchCommitTransaction();
+				System.out.println("Committed Transaction:"+agent().getCurrentTransactionID());
+			}
+			@Override
+			protected void dispatchFeedback(){
+				super.dispatchFeedback();
+				System.out.println("Dispatched Feedback"+agent().getCurrentTransactionID());
+			}
+			@Override
+			public void dispatchEntryAdded(ContainerEntry containerEntry){
+				super.dispatchEntryAdded(containerEntry);
 				s.countDown();
 			}
-
-			@Override
-			public void beginTran(TransactionEvent te) {
-				System.out.println("Received Transaction Started:"+te);
-				tran.acquireUninterruptibly();
-				super.beginTran(te);
-			}
-
-			@Override
-			public void commitTran(TransactionEvent te) {
-				System.out.println("Received Transaction Completed:"+te);
-				tran.release();
-				super.commitTran(te);
-			}
-			
 		};
-		source.agent().connect(new ConnectionEvent(SOURCE,SINK,agent));
-		agent.addFeedbackAgent(new FeedbackAgentImpl(SINK,source.agent()));
+		source.agent().connect(new ConnectionEvent(SOURCE,SINK,sink.agent()));
+		sink.agent().addFeedbackAgent(new FeedbackAgentImpl(SINK,source.agent()));
 		Thread t= new Thread(){
 			public void run(){
 				for(int i=0;i<lock;i++){
 					source.agent().entryAdded(new ContainerInsertEvent(SOURCE,new TransportEntry(5000000+i),0));
+					insertDelay();
 				}
 			}
 		};
 		t.start();
 		for(int i=0;i<lock;i++){
 			source.agent().entryAdded(new ContainerInsertEvent(SOURCE,new TransportEntry(3000000+i),0));
+			insertDelay();
 		}
 		s.await();
 		tran.acquire();
 		Thread.sleep(1000);
+	}
+
+	private void insertDelay() {
+		try {
+			Thread.sleep(0,100);
+		} catch (InterruptedException e) {
+		}
 	}
 }
