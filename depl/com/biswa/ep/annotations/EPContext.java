6b82117b6e7ff805d7c4f0ce126caea9ff38da88
Biswaranjan.Das <biswaranjan.das@gmail.com>
2013-04-07 23:31:08 +0000

    Javadocs for annotations.

--- depl/com/biswa/ep/annotations/EPContext.java
+++ depl/com/biswa/ep/annotations/EPContext.java
@@ -4,10 +4,23 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
+/**
+ * Annotates top level element in event plus framework. This is intercepted 
+ * by compiler and sent for code generation.
+ * @author Biswa
+ *
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
 public @interface EPContext {
+	/**
+	 * Specify the external contexts needs to be imported into the current context.
+	 * @return String[]
+	 */
 	String[] schemas() default {};
+	/**
+	 * Specify the java packages need to be imported into the current context.
+	 * @return String[]
+	 */
 	String[] packages() default {};
 }
