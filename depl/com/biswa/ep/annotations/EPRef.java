060ce4bec171115dbf217659ef76c13d32173169
Biswaranjan.Das <biswaranjan.das@gmail.com>
2013-04-07 23:36:14 +0000

    Javadocs for annotations.

--- depl/com/biswa/ep/annotations/EPRef.java
+++ depl/com/biswa/ep/annotations/EPRef.java
@@ -9,14 +9,33 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target( { ElementType.TYPE })
 public @interface EPRef {
+	/**
+	 * Name of the context this container is trying to inherit from. Defaults to
+	 * current context.
+	 * @return String
+	 */
 	String context() default "";
-	
+	/**
+	 * Name of the container being inherited.
+	 * @return String
+	 */
 	String container();
-
+	/**
+	 * Any filter to apply while inheriting.
+	 * @return String
+	 */
 	String filter() default "";
-	
+
+	/**
+	 * Filtering mode this container applies.
+	 * @return Mode
+	 */
 	Mode mode() default Mode.AND;
-	
+	/**
+	 * All available filtering modes.
+	 * @author Biswa
+	 *
+	 */
 	enum Mode{
 		AND,OR,NONE;
 	}
