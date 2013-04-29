0a844155255fd2926be26bdb939eaf5fd72fad4e
Biswaranjan.Das <biswaranjan.das@gmail.com>
2013-04-29 02:35:02 +0000

    Setting sourceAgent in Viewers for operation dispatching.

--- viewer/com/biswa/ep/util/Viewer.java
+++ viewer/com/biswa/ep/util/Viewer.java
@@ -7,6 +7,7 @@ import com.biswa.ep.entities.ConnectionEvent;
 public class Viewer extends GenericViewer {
 	public Viewer(ConcreteContainer cs,final Attribute ... attr) {
 		super(cs.getName());
+		setSourceAgent(cs.agent());
 		cs.agent().connect(new ConnectionEvent(cs.getName(),getName(),this.agent()));
 	}
 }
