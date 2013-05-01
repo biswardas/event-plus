d6018a123ebe7c814cfac5c491adba3da4c9d657
Biswaranjan.Das@gmail.com <Biswaranjan.Das@gmail.com@837f84b3-34e9-d943-8c0c-9cbd1e6a6e68>
2013-05-01 15:35:12 +0000

    Type framework for Dynamic Attribute's

--- trunk/depl/com/biswa/ep/provider/CompiledAttributeProvider.java
+++ trunk/depl/com/biswa/ep/provider/CompiledAttributeProvider.java
@@ -1,11 +1,13 @@
 package com.biswa.ep.provider;
 
+import java.util.Map;
+
 import com.biswa.ep.entities.Attribute;
 import com.biswa.ep.entities.dyna.DynamicAttributeProvider;
 
 public class CompiledAttributeProvider implements DynamicAttributeProvider {
 	@Override
-	public Attribute getAttribute(String expression) {
-		return new CompiledJavaObject(expression).getCompiledAttribute();
+	public Attribute getAttribute(String expression,Map<String,Class<? extends Object>> typeMap) {
+		return new CompiledJavaObject(expression,typeMap).getCompiledAttribute();
 	}
 }
