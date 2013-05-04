1fbfb12fda13cf4e701c3c88fa6adbccfe551ae7
Biswaranjan.Das@gmail.com <Biswaranjan.Das@gmail.com@837f84b3-34e9-d943-8c0c-9cbd1e6a6e68>
2013-05-04 13:18:56 +0000

    Bringing D3 into main stream..

--- trunk/core/com/biswa/ep/entities/store/ContainerStoreFactory.java
+++ trunk/core/com/biswa/ep/entities/store/ContainerStoreFactory.java
@@ -6,12 +6,6 @@ import com.biswa.ep.entities.PropertyConstants;
 public class ContainerStoreFactory {
 	public static ContainerEntryStore getContainerEntryStore(ConcreteContainer concreteContainer){
 		
-		boolean d3support=false;
-		String d3strProperty = concreteContainer.getProperty(PropertyConstants.D3_SUPPORT);
-		if(d3strProperty!=null){
-			d3support = Boolean.parseBoolean(d3strProperty);
-		}
-		
 		int passivation_idle_period = 0;
 		String passivateDurStrProperty = concreteContainer.getProperty(PropertyConstants.PASSIVATION_IDLE_DURATION);
 		if(passivateDurStrProperty!=null){
@@ -28,17 +22,9 @@ public class ContainerStoreFactory {
 			}else{
 				eager = true;
 			}		
-			if(d3support){
-				return new D3PassivableContainerEntryStore(concreteContainer,passivation_idle_period,eager);	
-			}else{
-				return new PassivableContainerEntryStore(concreteContainer,passivation_idle_period,eager);	
-			}		
-		}else{			
-			if(d3support){
-				return new D3ContainerEntryStore(concreteContainer);	
-			}else{
-				return new ConcreteContainerEntryStore(concreteContainer);
-			}
+			return new PassivableContainerEntryStore(concreteContainer,passivation_idle_period,eager);
+		}else{
+			return new ConcreteContainerEntryStore(concreteContainer);
 		}	
 	}
 }
