ae10369787baabd04dec02b2a0a3c3bd8095239a
Biswaranjan.Das@gmail.com <Biswaranjan.Das@gmail.com@837f84b3-34e9-d943-8c0c-9cbd1e6a6e68>
2013-05-11 17:30:54 +0000

    Improved passivation...

--- trunk/core/com/biswa/ep/entities/store/PersistableContainerEntry.java
+++ trunk/core/com/biswa/ep/entities/store/PersistableContainerEntry.java
@@ -1,13 +1,5 @@
 package com.biswa.ep.entities.store;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
@@ -29,8 +21,7 @@ class PersistableContainerEntry extends AbstractPhysicalEntry {
 					lastAccessed = System.currentTimeMillis();
 					if (underlyingEntry == proxyEntry) {
 						try {
-							underlyingEntry = readFromDisk((PassivableContainerEntryStore) getContainer()
-									.getContainerEntryStore());
+							underlyingEntry = getAssociatedStore().load(getIdentitySequence());
 						} catch (Exception e) {
 							throw new RuntimeException("Unable to restore passivated entry");
 						}
@@ -51,10 +42,10 @@ class PersistableContainerEntry extends AbstractPhysicalEntry {
 		return lastAccessed;
 	}
 
-	final protected boolean passivate(PassivableContainerEntryStore passiveStore) {
+	final protected boolean passivate() {
 		if (underlyingEntry != proxyEntry) {
 			try {
-				writeToDisk(passiveStore);
+				getAssociatedStore().store(underlyingEntry);
 				underlyingEntry = proxyEntry;
 				return true;
 			} catch (Exception e) {
@@ -64,51 +55,6 @@ class PersistableContainerEntry extends AbstractPhysicalEntry {
 		return false;
 	}
 
-	private void writeToDisk(PassivableContainerEntryStore passiveStore)
-			throws Exception {
-		ObjectOutputStream oos = null;
-		try {
-			File sourceFile = new File(passiveStore.getDirectory(), String
-					.valueOf(underlyingEntry.getIdentitySequence()));
-			BufferedOutputStream boos = new BufferedOutputStream(
-					new FileOutputStream(sourceFile));
-			oos = new ObjectOutputStream(boos);
-			oos.writeObject(underlyingEntry);
-		} finally {
-			if (oos != null) {
-				try {
-					oos.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
-
-	private ConcreteContainerEntry readFromDisk(
-			PassivableContainerEntryStore passiveStore) throws Exception {
-		ObjectInputStream ois = null;
-		File sourceFile = null;
-		try {
-			sourceFile = new File(passiveStore.getDirectory(), String
-					.valueOf(externalIdentity));
-			BufferedInputStream bis = new BufferedInputStream(
-					new FileInputStream(sourceFile));
-			ois = new ObjectInputStream(bis);
-			ConcreteContainerEntry readObject = (ConcreteContainerEntry) ois
-					.readObject();
-			return readObject;
-		} finally {
-			if (ois != null) {
-				try {
-					ois.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				sourceFile.delete();
-			}
-		}
-	}
 
 	@Override
 	public Object getSubstance(Attribute attribute) {
@@ -153,5 +99,10 @@ class PersistableContainerEntry extends AbstractPhysicalEntry {
 	@Override
 	public String toString() {
 		return underlyingEntry.toString();
+	}
+
+	private PassivableContainerEntryStore getAssociatedStore() {
+		return (PassivableContainerEntryStore) getContainer()
+				.getContainerEntryStore();
 	}
 }
