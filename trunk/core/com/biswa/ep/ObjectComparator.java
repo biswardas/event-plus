f681ad2b76a78e0c107811d24ef0ea258351f16e
biswaranjan.das@gmail.com <biswaranjan.das@gmail.com@837f84b3-34e9-d943-8c0c-9cbd1e6a6e68>
2013-05-14 18:47:49 +0000

    Object comparison improvements..

--- trunk/core/com/biswa/ep/ObjectComparator.java
+++ trunk/core/com/biswa/ep/ObjectComparator.java
@@ -3,18 +3,30 @@ package com.biswa.ep;
 import java.util.Comparator;
 
 public class ObjectComparator implements Comparator<Object>{
+	@SuppressWarnings("unchecked")
 	@Override
 	public int compare(Object o1Substance, Object o2Substance) {
 
 		int compareValue = 0;
 		if (o1Substance != null && o2Substance != null) {
-			if (o1Substance.getClass().isAssignableFrom(
-					Number.class)
-					&& o2Substance.getClass().isAssignableFrom(
-							Number.class)) {
-				compareValue = compare((Number) o1Substance,
-						(Number) o2Substance);
-			}else{
+			if (Number.class.isAssignableFrom(o1Substance.getClass())
+					&& Number.class.isAssignableFrom(o2Substance.getClass())) {
+				double d1 = ((Number) o1Substance).doubleValue();
+				double d2 = ((Number) o2Substance).doubleValue();
+				if(d1>d2){
+					compareValue=1;
+				}else if(d2>d1){
+					compareValue=-1;
+				}else{
+					compareValue=0;
+				}
+			}else if (Comparable.class.isAssignableFrom(o1Substance.getClass())
+					&& Comparable.class.isAssignableFrom(o2Substance.getClass())
+					&& o1Substance.getClass().isAssignableFrom(o2Substance.getClass())) {		
+				Comparable<Object> c1= (Comparable<Object>) o1Substance;	
+				Comparable<Object> c2= (Comparable<Object>) o2Substance;
+				compareValue = (c1.compareTo(c2));
+			}else{			
 				compareValue = o1Substance.toString().compareTo(o2Substance.toString());
 			}
 		} else if (o1Substance == null && o2Substance != null) {
