9898f6a41ac23f344459d44d8d207c51e558106a
Biswaranjan.Das@gmail.com <Biswaranjan.Das@gmail.com@837f84b3-34e9-d943-8c0c-9cbd1e6a6e68>
2013-11-28 20:08:39 +0000

    javadocs on the spec

--- trunk/core/com/biswa/ep/entities/spec/Spec.java
+++ trunk/core/com/biswa/ep/entities/spec/Spec.java
@@ -3,18 +3,39 @@ package com.biswa.ep.entities.spec;
 import java.io.Serializable;
 
 import com.biswa.ep.entities.ContainerListener;
-
+/**
+ * Framework for applying operations such as aggregation,filtering,
+ * sorting,collapsing in the container.
+ * 
+ * @author Biswa
+ *
+ */
 public abstract class Spec implements Serializable{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 128447626953091459L;
+	
 	private final String sinkName;
+	/**
+	 * Name of the container on which this spec will be applied.
+	 * @param sinkName String
+	 */
 	public Spec(String sinkName){
 		this.sinkName=sinkName;
 	}
-	public String getSinkName() {
+	
+	/**Returns name of the container.
+	 * 
+	 * @return String
+	 */
+	public final String getSinkName() {
 		return sinkName;
 	}
+	
+	/**Applies the spec on the passed container.
+	 * 
+	 * @param listener ContainerListener
+	 */
 	public abstract void apply(ContainerListener listener);
 }
