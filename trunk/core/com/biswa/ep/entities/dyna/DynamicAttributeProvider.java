f893691c87eb6d579757ffff3ab7308a3bebc049
Biswaranjan.Das@gmail.com <Biswaranjan.Das@gmail.com@837f84b3-34e9-d943-8c0c-9cbd1e6a6e68>
2013-11-28 21:21:21 +0000

    Dynamic Attribute Provider interface..

--- trunk/core/com/biswa/ep/entities/dyna/DynamicAttributeProvider.java
+++ trunk/core/com/biswa/ep/entities/dyna/DynamicAttributeProvider.java
@@ -3,7 +3,18 @@ package com.biswa.ep.entities.dyna;
 import java.util.Map;
 
 import com.biswa.ep.entities.Attribute;
-
+/**
+ * Framework to provide dynamic attribution on the container. Implementation of this
+ * allows to insert dynamic attribute in runtime. 
+ * @author Biswa
+ *
+ */
 public interface DynamicAttributeProvider {
+	/**
+	 * 
+	 * @param expression String Expression to be inserted in the container.
+	 * @param types Map attribute name and type to generate attribute.
+	 * @return Attribute dynamically generated attribute
+	 */
 	public Attribute getAttribute(String expression,Map<String,Class<? extends Object>> types);
 }
